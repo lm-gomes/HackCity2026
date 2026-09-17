978c0671788e1814414d037831d01d8db2bcae8a
guizera2k26 <guilherme.siqueira09@alu.ufc.br>
2026-09-17 01:54:02 -0300

    Login

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -1,7 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 function App() {
-  
+ return(
+ <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Login />}/>
+
+
+    </Routes>
+
+  </BrowserRouter> 
+ )
+
 
 }
 
