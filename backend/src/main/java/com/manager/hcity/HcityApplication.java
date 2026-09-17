4d0487da0a1e4bd06dec51aa11bfa3a115caee37
lmgomes <lm.moreiragomes@gmail.com>
2026-09-16 23:22:57 -0300

    backend initialized

---
+++ backend/src/main/java/com/manager/hcity/HcityApplication.java
@@ -0,0 +1,13 @@
+package com.manager.hcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcityApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/manager/hcity/HcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.hcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
